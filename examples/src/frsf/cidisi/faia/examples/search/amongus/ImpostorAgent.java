17ba41060951f7a883da215ab14206b4403badd5
Ariel Benz <>
2024-04-27 12:07:01 -0300

    Add amongus project

---
+++ examples/src/frsf/cidisi/faia/examples/search/amongus/ImpostorAgent.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2007-2009 Georgina Stegmayer, Milagros Gutiérrez, Jorge Roa,
+ * Luis Ignacio Larrateguy y Milton Pividori.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package frsf.cidisi.faia.examples.search.amongus;
+
+import frsf.cidisi.faia.agent.Perception;
+
+import frsf.cidisi.faia.agent.search.SearchBasedAgent;
+import frsf.cidisi.faia.agent.Action;
+import frsf.cidisi.faia.solver.search.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ImpostorAgent extends SearchBasedAgent {
+
+    public ImpostorAgent() {
+
+    }
+
+    /**
+     * This method is executed by the simulator to ask the agent for an action.
+     */
+    @Override
+    public Action selectAction() {
+
+        // Create the search strategy
+        DepthFirstSearch strategy = new DepthFirstSearch();
+
+        /**
+         * Another search strategy examples:
+         * 
+         * Depth First Search:
+         * DepthFirstSearch strategy = new DepthFirstSearch();
+         * 
+         * Breath First Search:
+         * BreathFirstSearch strategy = new BreathFirstSearch();
+         * 
+         * Uniform Cost:
+         * IStepCostFunction costFunction = new CostFunction();
+         * UniformCostSearch strategy = new UniformCostSearch(costFunction);
+         * 
+         * A Star Search:
+         * IStepCostFunction cost = new CostFunction();
+         * IEstimatedCostFunction heuristic = new Heuristic();
+         * AStarSearch strategy = new AStarSearch(cost, heuristic);
+         * 
+         * Greedy Search:
+         * IEstimatedCostFunction heuristic = new Heuristic();
+         * GreedySearch strategy = new GreedySearch(heuristic);
+         */
+
+        // Create a Search object with the strategy
+        Search searchSolver = new Search(strategy);
+
+        /* Generate an XML file with the search tree. It can also be generated
+         * in other formats like PDF with PDF_TREE */
+        searchSolver.setVisibleTree(Search.EFAIA_TREE);
+
+        // Set the Search searchSolver.
+        this.setSolver(searchSolver);
+
+        // Ask the solver for the best action
+        Action selectedAction = null;
+        try {
+            selectedAction =
+                    this.getSolver().solve(new Object[]{this.getProblem()});
+        } catch (Exception ex) {
+            Logger.getLogger(ImpostorAgent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // Return the selected action
+        return selectedAction;
+    }
+
+    /**
+     * This method is executed by the simulator to give the agent a perception.
+     * Then it updates its state.
+     * @param p
+     */
+    @Override
+    public void see(Perception p) {
+        this.getAgentState().updateState(p);
+    }
+}
